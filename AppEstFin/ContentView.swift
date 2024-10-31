a2a62a3030c6cda96fc1d18554cdc89379a6b338
Elib Martz <elibwk@outlook.com>
2024-10-31 13:30:06 -0600

    uj

--- AppEstFin/ContentView.swift
+++ AppEstFin/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worlds!")
+            Text("Hello, worldss!")
         }
         .padding()
     }
